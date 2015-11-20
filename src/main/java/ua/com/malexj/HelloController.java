b08180c09cb18be09e344fb35864529682d8cc5b
MAlex <alexmaximovj@gmail.com>
2015-11-20 22:03:45 +0200

    Initial commit

---
+++ src/main/java/ua/com/malexj/HelloController.java
@@ -0,0 +1,40 @@
+package ua.com.malexj;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+//@Controller
+//@RequestMapping("/welcome")
+//public class HelloController {
+//
+//    @RequestMapping(method = RequestMethod.GET)
+//    public String printWelcome(ModelMap model) {
+//        model.addAttribute("message", "Spring 3 MVC - Hello World");
+//        return "hello";
+//    }
+//}
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Spring 3 MVC Hello World");
+        //возвращаеться страница jsp
+        return "hello";
+    }
+
+    @RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        ModelAndView model = new ModelAndView();
+        //устанавливаеться страница jsp
+        model.setViewName("hello");
+        //Передаеться объект name
+        model.addObject("msg", name);
+        return model;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/jsp/test.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ION
+  Date: 20.11.2015
+  Time: 13:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+<h1>test.jsp</h1>
+</body>
+</html>
+
+
